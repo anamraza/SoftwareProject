4502040a609a70a9d15c4a9fe9ce05e258ea2fc1
anamraza <anamraza88@hotmail.com>
2014-10-31 20:20:25 -0400

    Create uploadForm.php

---
+++ uploadForm.php
@@ -0,0 +1,16 @@
+
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="ISO-8859-1">
+<title>Insert title here</title>
+</head>
+<body>
+
+<form action="insertdata.php" method="post" enctype="multipart/form-data">
+Please choose a file: <input type="file" name="uploadFile"><br>
+<input type="submit" name="submit" value="Upload File">
+</form>
+
+</body>
+</html>
